a7132b4b210c32cbf494140dcfd798699e0bb462
ahsanjaved-dev <ahsanjavedyousafzai555@gmail.com>
2025-12-08 23:11:04 +0500

    commit

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/dummydata')
 def dummydata():
-    return jsonify([{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}, {"id": 3, "name": "Jim"}, {"id": 4, "name": "Jill"}, {"id": 5, "name": "Jack"}, {"id": 6, "name": "Jill"}, {"id": 7, "name": "Jack"}, {"id": 8, "name": "Jill"}, {"id": 9, "name": "Jack"}, {"id": 10, "name": "Jill"}])   
+    return jsonify([{"id": 1, "name": "John"}, {"id": 2, "name": "Jane"}, {"id": 3, "name": "Jim"}, {"id": 4, "name": "Jill"}, {"id": 5, "name": "Jack"}, {"id": 6, "name": "Alex"}, {"id": 7, "name": "Leo"}, {"id": 8, "name": "frank"}, {"id": 9, "name": "robert"}, {"id": 10, "name": "will"}])   
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
